60b13a6e9aff1f15d923975c9ee1e8012b55e8d5
foodhype <brianschmitz.main@gmail.com>
2014-11-12 13:03:32 -0600

    stable. I love Rust so much.

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,34 @@ use std::io::net::addrinfo;
 use std::io::TcpStream;
 
 
-fn test_http_request(host: &str) {
-    println!("host: {}", host);
-
-    let dns_lookup_result = addrinfo::get_host_addresses(host);
-    match dns_lookup_result {
+fn http_get_request(host: &str, path: &str) {
+    match addrinfo::get_host_addresses(host) {
         Ok(ip_addresses) => {
-            let ip_address = format!("{}", ip_addresses[0]);
-           
-            let mut socket = TcpStream::connect(ip_address.as_slice(), 80).unwrap();
-            socket.write(b"GET /index.html HTTP/1.1\nHost: www.example.com\n\n");
-            let response = socket.read_to_end();
-            match response {
+            let ip_address = format!("{}", ip_addresses[0]);    
+
+            println!("Establishing TCP connection...");
+            let mut socket = TcpStream::connect(
+                    ip_address.as_slice(), 80).unwrap();
+
+            println!("Sending GET request...");
+            let get_request = "GET /".to_string() + path + " HTTP/1.1\n" +
+                    "Host: " + host + "\n\n";
+            match socket.write(get_request.as_bytes()) {
+                Ok(_) => {
+                    println!("Request sent successfully.");
+                },
+                Err(error) => {
+                    println!("Failed to send request: {}", error);
+                }
+            }
+
+            println!("Reading from socket...");
+            match socket.read_to_end() {
                 Ok(value) => {
                     println!("{}", String::from_utf8(value));
                 },
                 Err(error) => {
-                    println!("Error response: {}", error);
+                    println!("HTTP error response: {}", error);
                 }
             }
         },
@@ -32,8 +43,12 @@ fn test_http_request(host: &str) {
 
 
 fn main() {
-    for line in io::stdin().lines() {
-        test_http_request(line.unwrap().as_slice());
-    }
+    println!("Enter host:");
+    let host = io::stdin().read_line();
+    println!("Enter path:");
+    let path = io::stdin().read_line();
+    http_get_request(
+        host.unwrap().as_slice().trim_right(),
+        path.unwrap().as_slice().trim_right());
 }
 
